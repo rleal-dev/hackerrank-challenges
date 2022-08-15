e2d0d0cfb8c7d30c300bb5f9a46c81c2755ef90e
Rogerio Leal <rogerioleal1@gmail.com>
2022-08-15 16:27:13 -0300

    feat: Day 0: Hello World and Data Types

---
+++ Javascript/js10-hello-world.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/js10-hello-world/problem
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);   
+}
+
+greeting('Welcome to 10 Days of JavaScript!')
